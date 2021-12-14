139837b6a687c6be79daab2d4ebd88376912c795
prachigarg19 <prachigarg1923@gmail.com>
2021-12-15 02:00:23 +0530

    form validation added

---
+++ script/regex.js
@@ -0,0 +1,31 @@
+//email-validation
+email=document.getElementById('email')
+email.addEventListener('blur',()=>{
+    let reg2=/^(\S){1,64}@([a-zA-Z]+)\.([a-zA-Z]+)/;
+    let result = reg2.test(email.value);
+       
+       if (result == true) {
+   
+           email.classList.remove('is-invalid');
+           emailvalue=true;
+       }
+       else {
+           email.classList.add('is-invalid');
+           emailvalue=false;
+       }
+   });
+
+  //validation for sign up email
+  btn=document.getElementById('signbtn');
+  btn.addEventListener('click',(e)=>{
+      let newpassword=document.getElementById('spassword');
+      let confirmpassword=document.getElementById('cpassword');
+      if(newpassword===confirmpassword)
+      { 
+        newpassword.classList.remove('is-invalid');
+      }
+      else{
+          e.preventDefault();
+        newpassword.classList.add('is-invalid');
+      }
+  })
