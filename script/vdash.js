5a76baff60bfea2ed3283017dd332538c6ddbe30
prachigarg19 <prachigarg1923@gmail.com>
2021-12-15 16:47:38 +0530

    added interested button feature+interested ngo display

--- script/vdash.js
+++ script/vdash.js
@@ -6,7 +6,8 @@ shelters=[
         Timings:'9 am to 8pm',
         requirement:'Clerical Support',
         Contact:'12346578',
-        Image:'../Images/ngo-profile-logo.jpg'
+        Image:'../Images/ngo-profile-logo.jpg',
+        interested:'false'
     },
     {
         name:'Sanjay Gandhi Animal Care Center(SGACC)',
@@ -14,7 +15,8 @@ shelters=[
         Timings:'9 am to 8pm',
         Contact:'12346578',
         requirement:'Groomer',
-        Image:'../Images/ngo-profile-logo2.jpg'
+        Image:'../Images/ngo-profile-logo2.jpg',
+        interested:'false'
     },
     {
         name:'Animal Aid Unlimited',
@@ -22,7 +24,8 @@ shelters=[
         Timings:'9 am to 8pm',
         Contact:'12346578',
         requirement:'Clerical Support',
-        Image:'../Images/ngo-profile-logo.jpg'
+        Image:'../Images/ngo-profile-logo.jpg',
+        interested:'false'
     },
     {
         name:'Sanjay Gandhi Animal Care Center(SGACC)',
@@ -30,7 +33,8 @@ shelters=[
         Timings:'9 am to 8pm',
         Contact:'12346578',
         requirement:'Groomer',
-        Image:'../Images/ngo-profile-logo2.jpg'
+        Image:'../Images/ngo-profile-logo2.jpg',
+        interested:'false'
     },
     {
         name:'Animal Aid Unlimited',
@@ -38,7 +42,8 @@ shelters=[
         Timings:'9 am to 8pm',
         Contact:'12346578',
         requirement:'Clerical Support',
-        Image:'../Images/ngo-profile-logo.jpg'
+        Image:'../Images/ngo-profile-logo.jpg',
+        interested:'false'
     },
     {
         name:'Sanjay Gandhi Animal Care Center(SGACC)',
@@ -46,7 +51,8 @@ shelters=[
         Timings:'9 am to 8pm',
         Contact:'12346578',
         requirement:'Groomer',
-        Image:'../Images/ngo-profile-logo2.jpg'
+        Image:'../Images/ngo-profile-logo2.jpg',
+        interested:'false'
     },
     {
         name:'Animal Aid Unlimited',
@@ -54,7 +60,8 @@ shelters=[
         Timings:'9 am to 8pm',
         Contact:'12346578',
         requirement:'Clerical Support',
-        Image:'../Images/ngo-profile-logo.jpg'
+        Image:'../Images/ngo-profile-logo.jpg',
+        interested:'false'
     },
     {
         name:'Sanjay Gandhi Animal Care Center(SGACC)',
@@ -62,60 +69,87 @@ shelters=[
         Timings:'9 am to 8pm',
         Contact:'12346578',
         requirement:'Groomer',
-        Image:'../Images/ngo-profile-logo2.jpg'
+        Image:'../Images/ngo-profile-logo2.jpg',
+        interested:'false'
     }
     
 ];
 console.log('hello');
 list=document.getElementsByClassName('dropdown-list')
-
+// let interested=false;
 
 //compares area entered by user with all ngo's in database
 Array.from(list).forEach(item=>{
     item.addEventListener('click',(e)=>{
-        display(e.target.id);
+        document.getElementById('cards').innerHTML="";
+        check(e.target.id,'cards');
     })
     
 })
 
-//displaying all cards that matches area 
-function display(id){
-    card=document.getElementById('cards');
-    card.innerHTML="";
-    str="";
+//adding interested ngo's
+div2=document.getElementById('interested-container');
+
+//if user is interested
+function isInterested(index){
+    id=index;
+    console.log(shelters)
+    if(shelters[id].interested==='false')
+    {   ibtn=document.getElementById(`${id}`);
+        ibtn.style['background-color']="#93B5C6";
+        shelters[id].interested='true';
+        displayCards(id,'interested-container');
+        // console.log(shelters[id]);
+    }
+       
+}
+
+//checks all cards that matches area 
+function check(id){
+   
+    
     index=id;
-    for(shelter of shelters){
-        if(shelter['location']===id)
+    for(i=0;i<shelters.length;i++){
+        if(shelters[i]['location']===id)
         {
-            str+=` <div class="cards">
+            displayCards(i,'cards');
+        }
+       
+    }
+   
+}
+//displays card
+function displayCards(i,divid){
+    card=document.getElementById(`${divid}`);
+    // card.innerHTML="";
+    let str="";
+    str+=` <div class="cards">
             <div class="card-details">
-            <h2>${shelter.name}</h2>
+            <h2>${shelters[i].name}</h2>
             <h4>  <i class="fas fa-map-pin"></i>
-                <p>${shelter.location}</p>
+                <p>${shelters[i].location}</p>
             </h4>
             <h4>
                 <span>Work Requirment:</span>
-                <p> ${shelter.requirement}</p>
+                <p> ${shelters[i].requirement}</p>
             </h4>
             <h4>
                <span>Timings:</span> 
-                <p>${shelter.Timings}</p>
+                <p>${shelters[i].Timings}</p>
             </h4>
             <h4>
                 <span>Contact:</span>
-                <p>${shelter.Contact}</p>
+                <p>${shelters[i].Contact}</p>
             </h4>
             <h4 id="view-more">
                 <a href="../html/sprofile.html">View More ></a>
             </h4>
+            <button id="${i}" class="sign-up interest" onclick="isInterested(${i})">Interested</button>
         </div>
    
     <div class="card-logo">
-        <img src="${shelter.Image}" alt="ngo-logo" height="250px" width="250px">
+        <img src="${shelters[i].Image}" alt="ngo-logo" height="250px" width="250px">
     </div>
     </div>`;
-        }
-       
-    }
-    card.innerHTML=str;
+    card.innerHTML+=str;
 }
